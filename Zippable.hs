9e5a3bace38a11086acfc35bbdd6925bd89bfdde
Urska Pangerc <urska19@users.noreply.github.com>
2015-01-09 23:19:23 +0100

    dodana 'glava', instance

--- Zippable.hs
+++ Zippable.hs
@@ -1,7 +1,21 @@
-module Zippable  where
+{-|
+Module      : Zippable
+Description : Bidirectionalization
+Copyright   : (c) Urska, 2015;
+		  Melanija, 2015
+License     : GPL-3
+
+Stability   : experimental
+
+-- Class of data structures that can match equal shapes and combine the values.
+-}
+
+module Zippable
+      (Zippable,
+      tryZip)
+where
 
 import Control.Applicative
-
 
 class Zippable k where
   tryZip :: k a -> k b -> Either String (k (a,b))
@@ -12,7 +26,15 @@ instance Zippable [] where
 				    <*> tryZip xs ys
   tryZip _ _ = Left "Update changes the length."
   
---instance Zippable Maybe where
---  tryZip (Just a) (Just b) = Right (Just (a b))
---  tryZip _ _ = Right Nothing
+data Tree a = Leaf a | Node (Tree a) (Tree a)
 
+instance Zippable Tree where
+  tryZip (Leaf x) (Leaf y) = Right (Leaf (x,y))
+  tryZip (Node n m) (Node o p) = Right Node 
+				    <*> tryZip n o
+				    <*> tryZip m p
+  tryZip _ _ = Left "Update chances the shape"
+ 
+instance Zippable Maybe where
+  tryZip (Just a) (Just b) = Right (Just (a, b))
+  tryZip _ _ = Right Nothing
