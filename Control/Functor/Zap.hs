4c22d8993a3c1638d0a91aee0cadfdacfe9dff16
Urska Pangerc <urska.pangerc@gmail.com>
2015-01-15 18:04:11 +0100

    pomozne fje

---
+++ Control/Functor/Zap.hs
@@ -0,0 +1,19 @@
+-------------------------------------------------------------------------------------------
+-- |
+-- Module	: Control.Functor.Zap
+-- Copyright 	: 2008 Edward Kmett
+-- License	: BSD
+--
+-- Maintainer	: Edward Kmett <ekmett@gmail.com>
+-- Stability	: experimental
+-- Portability	: non-portable (functional-dependencies)
+--
+-- Dual (bi)Functors
+-------------------------------------------------------------------------------------------
+
+module Control.Functor.Zap 
+	( Zap(..), (>$<)
+	, Bizap(..), (>>$<<)
+	) where
+
+import Control.Functor.Internal.Adjunction
